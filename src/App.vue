f7ec7fe5399f2bada82472609412bca814414928
McKay Court <mcourt@simplenexus.com>
2023-03-23 18:15:57 -0600

    A bunch of stuff that we're going to delete later

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,54 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-
-  <main>
-      <HelloWorld msg="You did it!" />
-  </main>
+  <div>
+    <hello-world msg="You did it!" />
+    <div>{{ twoDigits }}</div>
+    <stuff
+        v-for="s in stuff"
+        :name="capitalizeFirstLetter(s)"
+    />
+  </div>
 </template>
 
-<style scoped>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Stuff from './components/Stuff/Stuff.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Stuff
+  },
+  created () {
+    this.stuff = [{
+      name: 'movies',
+    }, {
+      name: 'books',
+    }, {
+      name: 'music'
+    }]
+  },
+  computed: {
+    twoDigits () {
+      return this.something.toFixed(2)
+    }
+  },
+  methods: {
+    capitalizeFirstLetter ({ name }) {
+      const [firstLetter] = name
+      return `${firstLetter.toUpperCase()}${name.substring(1, name.length)}`
+    }
+  },
+  data () {
+    return {
+      stuff: [],
+      something: 1.45789847
+    }
+  }
+}
+</script>
 
+<style scoped>
+  .red-background {
+    background-color: grey;
+  }
 </style>
---
+++ src/components/Stuff/Stuff.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="m-4 p-4 border-black border-2 rounded-2xl shadow-2xl">
+    <div class="flex">
+      <div>
+        {{ name }}
+      </div>
+      <div>
+        Something else
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stuff",
+  props: {
+    name: String,
+    required: true
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
